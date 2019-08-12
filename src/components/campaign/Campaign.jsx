11c61f9f37c87192224e84881e1f6bf85b57c514
emilj858 <emiljuboori1@gmail.com>
2019-08-11 17:13:30 -0700

    minor login changes and campaign initial work

--- src/components/campaign/Campaign.jsx
+++ src/components/campaign/Campaign.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Input, Button } from 'semantic-ui-react';
 
 const Campaign = () => (
   <div className="component-wrapper">
     <h1 className="header-text">Campaign</h1>
-    <div>Campaigns are based on keywords you promote to your audience.</div>
+    <div>Campaigns are based on keywords you promote to your audience. People who enter your keyword will be automatically added to your audience and recieve an automated reponse.</div>
+    <div>Set Keyword</div>
+    <Input />
+    <div>Automated Response</div>
+    <Input />
+    <Button className="primary">Save </Button>
   </div>
     )
 
